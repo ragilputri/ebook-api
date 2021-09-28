0fa5fb9f4cc04899ddef0b88992a19c9e6dade63
ragilputri <3103119151@student.smktelkom-pwt.sch.id>
2021-09-27 20:58:51 -0700

    challenge

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthorController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,9 +22,16 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::get('me','AuthController@me');
 
-//Routes API
+//Routes Books
 Route::get('books','BookController@index');
 Route::post('books','BookController@store');
 Route::get('books/{id}','BookController@show');
 Route::put('books/{id}','BookController@update');
 Route::delete('books/{id}','BookController@destroy');
+
+//Routes Author
+Route::get('authors','AuthorController@index');
+Route::post('authors','AuthorController@store');
+Route::get('authors/{id}','AuthorController@show');
+Route::put('authors/{id}','AuthorController@update');
+Route::delete('authors/{id}','AuthorController@destroy');
